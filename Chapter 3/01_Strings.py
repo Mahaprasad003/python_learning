ff5314d5c92426b2bdce36f8d380939e5578c9fa
Mahaprasad <mahaprasad003@gmail.com>
2021-04-19 23:21:37 +0530

    Day 1

---
+++ Chapter 3/01_Strings.py
@@ -0,0 +1,9 @@
+a = 34
+b = "Harry"
+print(b)
+
+A = "This is a test"
+print(type(A))
+
+B = ''' Harry is a naughty boy '''
+print(B)
---
+++ Chapter 3/02_String_slicing.py
@@ -0,0 +1,16 @@
+Greeting = "Good evening, "
+name = 'Mahaprasad'
+# Concatenating two strings
+# print(Greeting + name)
+print(name[5])
+print(name[-2])
+
+# String slicing
+print(name[0:3])
+print(name[0:])
+print(name[0:5])
+print(name[0:-1])
+print(name[0:]) # This is the same as [0:10]
+print(name[:10]) # This is the same as [0:10]
+print(name[-1:-3])
+print(name[1:10:2])
