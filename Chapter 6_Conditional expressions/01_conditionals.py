5a6d54861c78244aa3383cb68a810b6ea1e7d2b5
Mahaprasad <mahaprasad003@gmail.com>
2021-04-21 13:42:40 +0530

    conditional statements

---
+++ Chapter 6_Conditional expressions/01_conditionals.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number:\n"))
+if (0<a<3):
+    print("This value lies between 0 & 3. ")
+elif (a<0):
+    print("This number is negative.")
+elif (a>0):
+    print("This value is positive.")
+
+
+
